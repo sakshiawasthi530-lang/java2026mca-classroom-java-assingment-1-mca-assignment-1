a961dab952cf3de921fa36663d1be33125b35d5f
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-06 21:55:05 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -4,22 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+for (int i = 1; i <= n; i++) {
+            
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+          
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
+       
+        for (int i = n - 1; i >= 1; i--) {
+           
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+           
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
