f5af1ee9847760455aed3ff4e2bec643e3e784f8
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-07 18:31:48 +0530

    Bubble Sort Main.java

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,24 +1,16 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int[] arr = new int[n];
-
-        
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        int swaps = 0;
-
-       
+        int swaps = 0;       
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -26,17 +18,13 @@ public class Main {
                 }
             }
         }
-
-        
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]);
             if (i != n - 1) {
                 System.out.print(" ");
             }
         }
-        System.out.println();
-
-        
+        System.out.println();        
         System.out.println("Swaps: " + swaps);
     }
 }
