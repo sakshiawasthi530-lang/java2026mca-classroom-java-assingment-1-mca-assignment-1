cb849fbc8dda0bbed173eee66b0aaf73e05725e7
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-07 11:04:06 +0530

    Add spiral matrix filling and diagonal sum calculation
    Implemented a method to fill an N×N matrix in clockwise spiral order and print the primary diagonal sum.

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -5,16 +5,58 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        int[][] arr = new int[n][n];
 
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+
+            // Top row
+            for (int i = left; i <= right; i++) {
+                arr[top][i] = num++;
+            }
+            top++;
+
+            
+            for (int i = top; i <= bottom; i++) {
+                arr[i][right] = num++;
+            }
+            right--;
+
+           
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    arr[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+           
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    arr[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]);
+                if (j < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        
+        int diagonalSum = 0;
+        for (int i = 0; i < n; i++) {
+            diagonalSum += arr[i][i];
+        }
+
+        System.out.println("Diagonal: " + diagonalSum);
     }
 }
