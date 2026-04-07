f0ebc5af89e830e22969579edd553ae5234e6e09
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-07 18:28:45 +0530

    Shape Area Main.java

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -1,12 +1,9 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String shape = sc.next();
-
         double area = 0;
-
         if (shape.equals("circle")) {
             double r = sc.nextDouble();
             area = Math.PI * r * r;
@@ -15,16 +12,13 @@ public class Main {
             double l = sc.nextDouble();
             double w = sc.nextDouble();
             area = l * w;
-
         } else if (shape.equals("triangle")) {
             double a = sc.nextDouble();
             double b = sc.nextDouble();
             double c = sc.nextDouble();
-
             double s = (a + b + c) / 2.0;
             area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
         }
-
         System.out.printf("Area: %.2f\n", area);
     }
 }
