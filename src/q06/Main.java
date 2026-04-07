1dcc04f8c96c8b5c45fc5e68b5eea6cdab4f4bd8
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-07 18:29:57 +0530

    Fibonacci Series Main.java
    Removed unnecessary whitespace and cleaned up formatting.

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -1,19 +1,14 @@
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int a = 0, b = 1;
-
         for (int i = 0; i < n; i++) {
             System.out.print(a);
-
             if (i != n - 1) {
                 System.out.print(" ");
             }
-
             int c = a + b;
             a = b;
             b = c;
