e788286abbe38068a4145b8a5d34e47922d161ff
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-07 11:54:53 +0530

    Add postfix expression evaluation in Main.java
    Implemented evaluation of postfix expressions using a stack.

--- src/q10/Main.java
+++ src/q10/Main.java
@@ -3,19 +3,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // TODO: Evaluate a postfix (Reverse Polish Notation) expression
-        //       Operands are non-negative integers, operators are: + - * /
-        //       Use a stack to evaluate
-        //       Input: single line, tokens separated by spaces
-        //       Output: integer result
-        //
-        // Input: 5 1 2 + 4 * + 3 -
-        // Output: 14
-        //
-        // Explanation: 5 + ((1+2)*4) - 3 = 5 + 12 - 3 = 14
-
         String line = sc.nextLine();
 
+        Stack<Integer> stack = new Stack<>();
+        String[] tokens = line.split(" ");
+
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a + b);
+            } else if (token.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a * b);
+            } else if (token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        System.out.println(stack.pop());
     }
 }
