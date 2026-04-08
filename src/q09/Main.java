dbb25de3b693837a009fe4da6ff717193aebdfdd
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-08 12:40:23 +0530

    matrix multiplication Main.java
    Removed commented sections for input and output of matrices.

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -4,26 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         int[][] A = new int[n][n];
         int[][] B = new int[n][n];
         int[][] C = new int[n][n];
-
-        // Input matrix A
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
-
-        // Input matrix B
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
-
-        // Multiply matrices: C = A * B
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 C[i][j] = 0;
@@ -32,8 +26,6 @@ public class Main {
                 }
             }
         }
-
-        // Print result matrix C
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(C[i][j]);
