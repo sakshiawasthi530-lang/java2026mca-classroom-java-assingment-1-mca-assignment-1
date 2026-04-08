8aafe0c7f92322ef5cea695e439b8fe7b1ed01a4
sakshiawasthi530-lang <sakshiawasthi530@gmail.com>
2026-04-08 13:53:39 +0530

    Hollow Diamond  Main.java
    Removed comments for leading spaces and hollow stars in diamond pattern printing.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,17 +1,14 @@
-
 import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
      for (int i = 1; i <= n; i++) {
-            // Print leading spaces
+            
             for (int j = i; j < n; j++) {
                 System.out.print(" ");
-            }
-            // Print hollow stars
+            }  
             for (int j = 1; j <= (2 * i - 1); j++) {
                 if (j == 1 || j == (2 * i - 1)) {
                     System.out.print("*");
@@ -21,14 +18,10 @@ public class Main {
             }
             System.out.println();
         }
-
-        // Lower half of the diamond
         for (int i = n - 1; i >= 1; i--) {
-            // Print leading spaces
             for (int j = n; j > i; j--) {
                 System.out.print(" ");
             }
-            // Print hollow stars
             for (int j = 1; j <= (2 * i - 1); j++) {
                 if (j == 1 || j == (2 * i - 1)) {
                     System.out.print("*");
